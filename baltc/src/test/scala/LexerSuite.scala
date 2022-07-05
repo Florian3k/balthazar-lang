af3aefc862aa85d46b93097458cb80c94d53c755
brych <brychlikow@gmai.com>
2022-07-05 19:46:04 +0200

    add modulo and shift operators

--- baltc/src/test/scala/LexerSuite.scala
+++ baltc/src/test/scala/LexerSuite.scala
@@ -10,3 +10,34 @@ class LexerSuite extends munit.FunSuite:
     )
     assertEquals(res, expected)
   }
+
+  test("recognize shift operators") {
+    val res = lexer(">> <<", "file1")
+    val expected = List(
+      Token(ShiftRight, ">>", Location("file1", 1, 1)),
+      Token(ShiftLeft,  "<<", Location("file1", 1, 4))
+    )
+    assertEquals(res, expected)
+  }
+
+  test("differentiate between comparissons and shifts") {
+    val res = lexer("> > >> << < << <", "file1")
+    val expected = List (
+      Token(GreaterThan, ">", Location("file1", 1, 1)),
+      Token(GreaterThan, ">", Location("file1", 1, 3)),
+      Token(ShiftRight, ">>", Location("file1", 1, 5)),
+      Token(ShiftLeft, "<<", Location("file1", 1, 8)),
+      Token(LessThan, "<", Location("file1", 1, 11)),
+      Token(ShiftLeft, "<<", Location("file1", 1, 13)),
+      Token(LessThan, "<", Location("file1", 1, 16)),
+    )
+    assertEquals(res, expected)
+  }
+
+  test("lex modulo operator") {
+    val res = lexer("%", "file1")
+    val expected = List (
+      Token(Modulo, "%", Location("file1", 1, 1))
+    )
+    assertEquals(res, expected)
+  }
